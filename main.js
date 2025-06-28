0b1e7c9fffafd7c2d98ed01e94611f4ac66b0da5
Anthony <anthonydfarris@gmail.com>
2025-06-27 23:53:28 -0700

    Added code to load a string of words onto the typing text area. Each letter element is given data for use later when typing.

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 'use strict';
 
-document.addEventListener('keydown', (e) => {
-	console.log(e.key);
-});
+let currCharPos = 0;
+
+function initializeTypingTextArea() {
+	const typingTextArea = document.getElementById('typing-text-area');
+
+	if (!words) {
+		throw new Error(`"words" array expected but not found! Did "words.js" load correctly?`);
+	}
+
+	const randomIndex = Math.floor(Math.random() * words.length);
+	const wordsSelection = words[randomIndex];
+	let charPos = 0;
+
+	wordsSelection.split('').forEach((character) => {
+		const newCharacterSpan = document.createElement('span');
+		newCharacterSpan.classList.add('typed-ready');
+		newCharacterSpan.setAttribute('data-character', character);
+		newCharacterSpan.setAttribute('data-character-pos', charPos);
+		newCharacterSpan.innerHTML = character;
+		typingTextArea.appendChild(newCharacterSpan);
+		charPos += 1;
+	});
+}
+
+window.onload = () => {
+	initializeTypingTextArea();
+
+	document.addEventListener('keydown', (e) => {
+		console.log(e.key);
+	});
+};
